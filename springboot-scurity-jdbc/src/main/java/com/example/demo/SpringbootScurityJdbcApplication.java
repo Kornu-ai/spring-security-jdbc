d88d68347de49b6a55bbb4940f611e461e5c5a61
babji <babji@DESKTOP-2VFRJ0D>
2021-05-10 16:01:22 +0530

    initial commit

---
+++ springboot-scurity-jdbc/src/main/java/com/example/demo/SpringbootScurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootScurityJdbcApplication.class, args);
+	}
+
+}
